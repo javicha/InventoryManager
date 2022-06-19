a02953f4527a22f833a3d566d4bdb820213cc420
Javier Val <javilanhas@gmail.com>
2022-06-19 12:22:57 +0200

    Configure MassTransit with RabbitMQ connection

--- src/Services/Accounting/Accounting.API/Program.cs
+++ src/Services/Accounting/Accounting.API/Program.cs
@@ -1,8 +1,19 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+//WebApplicationBuilder returned by WebApplication.CreateBuilder(args) exposes Configuration and Environment properties
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add MassTransit and configure it with RabbitMQ connection
+builder.Services.AddMassTransit(config => {
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(configuration["EventBusSettings:HostAddress"]);
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
