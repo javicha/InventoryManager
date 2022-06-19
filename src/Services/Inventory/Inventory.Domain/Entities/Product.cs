a38c3526dea3849d51ca4f594c361c1a9371a32a
javier.val <javier.val@tiendeo.com>
2022-06-19 23:15:38 +0200

    Domain test

--- src/Services/Inventory/Inventory.Domain/Entities/Product.cs
+++ src/Services/Inventory/Inventory.Domain/Entities/Product.cs
@@ -55,7 +55,7 @@ namespace Inventory.Domain.Entities
 
         public Product() { }
 
-        public Product(string name, string reference, string description, int? typeId, decimal? basePrice, int? manufacturerId, int numUnits, 
+        public Product(string name, string reference, string description, int? typeId, decimal? basePrice, int? manufacturerId, int numUnits,
             int? minStock, int? supplierId, DateTime receiptDate, DateTime? expirationDate,
             string userCreated)
         {
@@ -73,6 +73,38 @@ namespace Inventory.Domain.Entities
             UserCreated = userCreated;
             UserModified = userCreated;
         }
+
+
+        #region PublicMethods
+
+        /*
+         * For illustrative purposes only. We do not use these methods. 
+         * They are an example of business logic encapsulated in the domain, following the DDD principles
+         */
+
+        /// <summary>
+        /// Decrements the number of units of the product. The minimum number of units will be zero.
+        /// </summary>
+        /// <param name="quantity">Number of units to decrease</param>
+        public void DecreaseNumUnits(int quantity)
+        {
+            NumUnits -= quantity;
+            if (NumUnits < 0) { NumUnits = 0; }
+        }
+
+        /// <summary>
+        /// Increase the number of units of the product
+        /// </summary>
+        /// <param name="quantity">Number of units to increase</param>
+        public void IncreaseNumUnits(int quantity)
+        {
+            if (quantity > 0)
+            {
+                NumUnits += quantity;
+            }
+        }
+
+        #endregion
     }
 }
 #pragma warning restore CS8618
---
+++ test/Inventory.Tests/Domain/Products/ProductTest.cs
@@ -0,0 +1,41 @@
+﻿using Inventory.Domain.Entities;
+
+namespace Inventory.Tests.Domain.Products
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void Product_Create()
+        {
+            var name = "Product Test";
+            var reference = "Ref-test";
+            var description = "Test product";
+            var typeId = 1;
+            var basePrice = 700;
+            var manufacturerId = 0;
+            var numUnits = 1;
+            var minStock = 1;
+            var supplierId = 2;
+            var receiptDate = System.DateTime.Today;
+            var expirationDate = System.DateTime.Today.AddYears(3);
+            var userCreated = "user.test";
+
+            var product = new Product(name, reference, description, typeId, basePrice,
+                manufacturerId, numUnits, minStock, supplierId, receiptDate, expirationDate, userCreated);
+
+            Assert.Equal(name, product.Name);
+            Assert.Equal(reference, product.Reference);
+            Assert.Equal(description, product.Description);
+            Assert.Equal(typeId, product.TypeId);
+            Assert.Equal(basePrice, product.BasePrice);
+            Assert.Equal(manufacturerId, product.ManufacturerId);
+            Assert.Equal(numUnits, product.NumUnits);
+            Assert.Equal(minStock, product.MinStock);
+            Assert.Equal(supplierId, product.SupplierId);
+            Assert.Equal(receiptDate, product.ReceiptDate);
+            Assert.Equal(expirationDate, product.ExpirationDate);
+            Assert.Equal(userCreated, product.UserCreated);
+            Assert.Equal(userCreated, product.UserModified);
+        }
+    }
+}
