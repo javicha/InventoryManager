9bdabe44d18a55242860ef1787c3221666d2bbe0
javier.val <javier.val@tiendeo.com>
2022-06-19 20:39:01 +0200

    Test project

---
+++ test/Inventory.Tests/Application/Products/CommandHandlers/AddProductHandlerTest.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Inventory.Application.Common.Enums;
+using Inventory.Application.Contracts.Persistence;
+using Inventory.Application.Features.Products.Commands.AddProduct;
+using Inventory.Application.Mappings;
+using Inventory.Domain.Entities;
+using Moq;
+using Moq.AutoMock;
+
+namespace Inventory.Tests.Application.Products.CommandHandlers
+{
+    public class AddProductHandlerTest
+    {
+        private readonly AutoMocker mocker;
+        private readonly AddProductCommandHandler handler;
+        private readonly Mock<IProductRepository> productRepository;
+
+        public AddProductHandlerTest()
+        {
+            mocker = new AutoMocker();
+            var mapperConfig = new MapperConfiguration(cfg => { cfg.AddProfile<MappingProfile>(); });
+            mocker.Use(mapperConfig.CreateMapper());
+            handler = mocker.CreateInstance<AddProductCommandHandler>();
+            productRepository = mocker.GetMock<IProductRepository>();
+        }
+
+        [Fact]
+        public async Task AddProductHandler_OK()
+        {
+            //Variables
+            Product newProduct = GetProduct();
+            var command = new AddProductCommand("Test", "Ref-test", "Test product", ProductTypeEnum.Primer, 400, 
+                ProductManufacturerEnum.Agilent, 5, null, ProductSupplierEnum.Supplier1, DateTime.Today, DateTime.Today.AddYears(1));
+
+            //Stub
+            this.productRepository.Setup(r => r.AddAsync(It.IsAny<Product>(), It.IsAny<string>())).ReturnsAsync(newProduct);
+
+            //Execute
+            var result = await this.handler.Handle(command, new CancellationToken());
+
+            //Assert
+            Assert.NotNull(result);
+
+            Assert.Equal(command.Name, newProduct.Name);
+            Assert.Equal(command.Reference, newProduct.Reference);
+            Assert.Equal(command.ExpirationDate, newProduct.ExpirationDate);
+
+            //Verify
+            productRepository.Verify(r => r.AddAsync(It.IsAny<Product>(), It.IsAny<string>()), Times.Once(), "Repository not called");
+        }
+
+        private Product GetProduct()
+        {
+            return new Product("Test", "Ref-test", "Test product", 3, 700, 0, 1, null, 2, DateTime.Today.AddDays(-1), DateTime.Today.AddYears(1), "user.test");
+        }
+    }
+}
---
+++ test/Inventory.Tests/Application/Products/CommandHandlers/RemoveProductByNameHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿using Inventory.Application.Contracts.Persistence;
+using Inventory.Application.Features.Products.Commands.RemoveProductByName;
+using Inventory.Domain.Entities;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+
+namespace Inventory.Tests.Application.Products.CommandHandlers
+{
+    public class RemoveProductByNameHandlerTest
+    {
+        private readonly AutoMocker mocker;
+        private readonly RemoveProductByNameCommandHandler handler;
+        private readonly Mock<IProductRepository> productRepository;
+
+        public RemoveProductByNameHandlerTest()
+        {
+            mocker = new AutoMocker();
+            handler = mocker.CreateInstance<RemoveProductByNameCommandHandler>();
+            productRepository = mocker.GetMock<IProductRepository>();
+        }
+
+        [Fact]
+        public async Task RemoveProductByNameHandler_OK()
+        {
+            //Variables
+            var command = new RemoveProductByNameCommand("Product Test");
+            var productToDelete = GetProduct();
+
+            //Stub
+            productRepository.Setup(r => r.GetByName(It.IsAny<string>())).ReturnsAsync(productToDelete);
+
+            //Execute
+            var result = await this.handler.Handle(command, new CancellationToken());
+
+            //Assert
+            Assert.Equal(Unit.Value, result);
+
+            //Verify
+            productRepository.Verify(r => r.GetByName(It.IsAny<string>()), Times.Once());
+            productRepository.Verify(r => r.SoftDeleteAsync(productToDelete, It.IsAny<string>()), Times.Once());
+        }
+
+        private Product GetProduct()
+        {
+            return new Product("Product Test", "Ref-test", "Test product", 3, 700, 0, 1, null, 2, DateTime.Today.AddDays(-1), DateTime.Today.AddYears(1), "user.test");
+        }
+    }
+}
